3750568274e61ffbb7c464e9368165a094a2ed01
Samat Ergeshmamet uulu <ergeshmametuulusamat@gmail.com>
2021-12-26 11:16:07 +0300

    Added calculator project

---
+++ script.js
@@ -0,0 +1,25 @@
+const bill = document.querySelector("#bill"); //input
+const service = document.querySelector("#service"); //select with options
+const people = document.querySelector("#people") //input
+const result = document.querySelector("#result")
+const each = document.querySelector("#each")
+const button = document.querySelector("#calculate")
+const tip = document.querySelector("#tipP")
+const inputs = document.querySelectorAll("input")
+
+button.addEventListener("click", calculate)
+
+function calculate() {
+  let index = service.selectedIndex;
+  let totalTip = Number(bill.value)*Number(service.options[index].value)/100
+  result.style.visibility = "visible";
+  tip.style.visibility = "visible";
+  
+  if (people.value > 1) {
+    result.innerHTML = (totalTip/Number(people.value)).toFixed(2)
+    each.style.visibility = "visible"
+  } else {
+    result.innerHTML = totalTip.toFixed(2)
+    each.style.visibility = "visible"
+  }
+}
